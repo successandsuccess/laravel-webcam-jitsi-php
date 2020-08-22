e3113f060c11ee3fa0c61d828948a6d97787ab8c
Raffale <javelinmcdestro@gmail.com>
2020-08-22 22:21:40 +0800

    mail test sec

--- resources/views/emails/meetNotifyMail.blade.php
+++ resources/views/emails/meetNotifyMail.blade.php
@@ -32,7 +32,7 @@
         <h3 class="mail-h3">Patient Phone: <span class="mail-span">{{ $details['phone'] }}</span></h3>
         <h3 class="mail-h3">Meet URL: <span class="mail-span"><a href="<?php echo $details['meetUrl']; ?>">{{ $details['meetUrl'] }}</a></span></h3>
         <h3 class="mail-h3">Meet Time: <span class="mail-span">{{ $details['time'] }}</span></h3>
-        <h2 class="mail-h2">When you are available, Please meet Patient with <a class="mail-red" href="<?php echo $details['meetUrl']; ?>">clicking Here.</a></h2>
+        <h2 class="mail-h2">When you are available, Please meet Patient with <a class="mail-red" style="color:red;" href="<?php echo $details['meetUrl']; ?>">clicking Here.</a></h2>
         <h2>Thank you.</h2>
     </div>
     
