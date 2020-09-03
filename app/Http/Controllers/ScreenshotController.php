efc2d5478fdda512f4c7f4a1e91809e6c160360e
Raffale <javelinmcdestro@gmail.com>
2020-09-03 05:51:49 +0200

    10

--- app/Http/Controllers/ScreenshotController.php
+++ app/Http/Controllers/ScreenshotController.php
@@ -16,9 +16,11 @@ class ScreenshotController extends Controller
     {
         // dd($request->input('uuid'));
         // $decodedImage = base64_decode($request->input('image'));
+        $data = $request->input('data');
         $img = $request->input('image');
         $uuid = $request->input('uuid');
-        print_r($img);
+        print_r($data);
+        print_r('<br>'.$img);
         print_r('<br>'.$uuid);
         print_r(222);
         exit;
