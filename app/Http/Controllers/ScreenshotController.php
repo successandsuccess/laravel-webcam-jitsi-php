6273b37e1115b66168f76530d56fdde3db5b3f48
Raffale <javelinmcdestro@gmail.com>
2020-09-03 05:27:26 +0200

    5

--- app/Http/Controllers/ScreenshotController.php
+++ app/Http/Controllers/ScreenshotController.php
@@ -24,6 +24,7 @@ class ScreenshotController extends Controller
 
         $image_parts = explode(";base64,", $img);
         $image_type_aux = explode("image/", $image_parts[0]);
+        dd($image_type_aux);
         $image_type = $image_type_aux[1];
         $image_base64 = base64_decode($image_parts[1]);
         $file = $folderPath. '/' . $uuid . '.'.$image_type;
