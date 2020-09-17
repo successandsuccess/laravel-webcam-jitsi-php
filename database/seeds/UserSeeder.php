6762214e787229e8f1a301cfcb9f9f241a5444a9
CodeStar <javelinmcdestro@gmail.com>
2020-09-18 00:56:06 +0200

    userSeeder update

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
+use Faker\Factory as Faker;
 
 class UserSeeder extends Seeder
 {
@@ -13,36 +14,42 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert(
-            [
+        $faker = Faker::create();
+
+        foreach (range(1, 10) as $index) 
+        {
+            DB::table('users')->insert(
                 [
-                    'name' => 'Rachel',
-                    'email' => Str::random(10).'@gmail.com',
-                    'email_verified_at' => now(),
-                    'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-                    'remember_token' => Str::random(10),
-                    'lname' => 'Green',
-                    'street' => 'Lightning 12',
-                    'no' => 12,
-                    'city' => 'Washington',
-                    'zip' => '11111',
-                    'insurance_carrier' => 'ambulance bus',
-                    'insurance_phone' => 12345678,
-                    'group' => 'patient',
-                    'policy_id' => 1728392,
-                    'phone' => 8766521,
-                    'prov_id_1' => 1,
-                    'prov_id_2' => 2,
-                    'prov_id_3' => 3,
-                    'dx_1' => 1,
-                    'dx_2' => 2,
-                    'dx_3' => 3,
-                    'dx_4' => 4,
-                    'dx_5' => 5,
-                    'created_at' => now(),
-                    'updated_at' => now()
+                    [
+                        'name' => $faker->name,
+                        'email' => $faker->unique()->safeEmail,
+                        'email_verified_at' => now(),
+                        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+                        'remember_token' => Str::random(10),
+                        'lname' => 'Green',
+                        'street' => 'Lightning 12',
+                        'no' => 12,
+                        'city' => 'Washington',
+                        'zip' => '11111',
+                        'insurance_carrier' => 'ambulance bus',
+                        'insurance_phone' => 12345678,
+                        'group' => 'patient',
+                        'policy_id' => 1728392,
+                        'phone' => 8766521,
+                        'prov_id_1' => 1,
+                        'prov_id_2' => 2,
+                        'prov_id_3' => 3,
+                        'dx_1' => 1,
+                        'dx_2' => 2,
+                        'dx_3' => 3,
+                        'dx_4' => 4,
+                        'dx_5' => 5,
+                        'created_at' => now(),
+                        'updated_at' => now()
+                    ]
                 ]
-            ]
-        );
+            );
+        }
+     
     }
 }
