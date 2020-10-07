1c2ced3977e2cf7abd11164293ca178dcddd755e
CodeStar <javelinmcdestro@gmail.com>
2020-10-07 17:59:22 +0200

    get started new design with chart

--- resources/views/patient/getstarted.blade.php
+++ resources/views/patient/getstarted.blade.php
@@ -147,7 +147,121 @@
                         </div>
                     </div>
                 </div>
+
+                <div class="row mt-35px">
+                    <div class="col-md-12">
+                        <div class="patient-box pt-10px">
+                            <p class="patient-bold-blue-p">History</p>
+
+                            <div class="row">
+                                <div class="col-md-3">
+                                    <div class="history-card">
+                                        <p class="history-brandon-font">Total Sessions completed</p>
+                                        <h3 class="history-brandon-big-font mb-0">14</h3>
+                                    </div>
+                                </div>
+                                <div class="col-md-3">
+                                    <div class="history-card">
+                                        <p class="history-brandon-font">Weekly Session Completed</p>
+                                        <h3 class="history-brandon-big-font mb-0">3</h3>
+                                    </div>
+                                </div>
+                                <div class="col-md-3">
+                                    <div class="history-card">
+                                        <p class="history-brandon-font">Average Session Length</p>
+                                        <h3 class="history-brandon-big-font mb-0">23 min</h3>
+                                    </div>
+                                </div>
+                                <div class="col-md-3">
+                                    <div class="history-card pl-0 pr-0">
+                                        <p class="history-brandon-font">Consecutive session streak</p>
+                                        <h3 class="history-brandon-big-font mb-0">3 days</h3>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <p class="patient-bold-blue-p mt-15px">Your Sessions Over Time</p>
+                            <div>
+                                <img src="{{ asset('admin_assets/dist/img/patternedchart.png') }}" alt="chart">
+                            </div>
+
+                            <p class="patient-bold-blue-p">Your Pain Over Time</p>
+
+                            <p class="exercise-blue-small-font mb-35px">09/28/2020 - 10/04/2020 &nbsp;<i class="fas fa-angle-down"></i></p>
+                            <canvas id="myChart" height="200" width="700"></canvas>
+                        </div>
+                    </div>
+                </div>
                 
             </div>
         </section>
+@endsection
+
+@section('javascript')
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js" integrity="sha512-s+xg36jbIujB2S2VKfpGmlC3T5V2TF3lY48DX7u2r9XzGzgPsa6wTpOQA7J9iffvdeBN0q9tKzRxVxw1JviZPg==" crossorigin="anonymous"></script>
+<script>
+var ctx = document.getElementById('myChart');
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['','Mon','Tue','Wed','Thur','Fri','Sat','Sun',''],
+        datasets: [{ 
+            data: [2,2,2,2,1,2,1,1,1],
+            label: "weak",
+            borderColor: "#4681ec", // blue
+            backgroundColor: "rgb(70, 129, 236, 0.6)",
+            fill: 'start'
+        }, { 
+            data: [4,4,3,3,2,2,2,2,2],
+            label: "strong",
+            borderColor: "#ff9e58", // dark yellow
+            backgroundColor: "rgb(255, 158, 88, 0.6)",
+            fill: 'start'
+        }
+        ]
+    },
+    options: {
+        responsive: false,
+        bezierCurve: false,
+        elements: {
+            line: {
+                tension: 0
+            }
+        },
+        scales: {
+            xAxes: [{
+                gridLines: {
+                    display: false
+                }
+            }],
+            yAxes: [{
+                stacked: false,
+				display: true,
+				scaleLabel: {
+					show: true
+				},
+				gridLines:{
+                    // color:"#ecedef",
+                    display: false,
+				},
+				ticks: {
+                    min: 0,
+                    max: 5,
+					beginAtZero:true,
+					stepSize: 1,
+					fontColor:"#8f9092",
+					callback:function(value) { 
+					    var x = ["", "No Pain", "Mild", "Moderate", "Intense", "Unspeakable"];
+							return x[value];
+					}
+				}
+			}]
+        },
+        legend: {
+            display: false,
+        }
+       
+    }
+});
+</script>
 @endsection
