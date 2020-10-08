c17ceaa4333570bc2a6c63d11effedf411a18426
CodeStar <javelinmcdestro@gmail.com>
2020-10-08 09:25:39 +0200

    care plan page update

--- resources/views/patient/careplan.blade.php
+++ resources/views/patient/careplan.blade.php
@@ -4,14 +4,15 @@
 <style>
 .progressbar {
     counter-reset: step;
+    margin-left: 135px;
 }
 .progressbar li {
     list-style-type: none;
     float: left;
-    width: 33.33%;
+    width: 25%;
     position: relative;
     text-align: center;
-    z-index: 22222;
+    color: #3746B0;
 }
 
 .progressbar li:before {
@@ -26,7 +27,6 @@
     margin: 0 auto 10px auto;
     border-radius: 50%;
     background-color: white;
-    z-index: 22222;
 }
 .progressbar li:after{
     content: '';
@@ -36,25 +36,39 @@
     background-color: #3746B0;
     top:15px;
     left: -50%;
-    /* z-index: -1; */
+    z-index: -1;
 }
 
 .progressbar li:first-child:after{
     content: none;
 }
 
+.progressbar li.active:before{
+    border:none;
+    background-color:#3746B0;
+    color: white;
+}
+
+.progressbar li.active {
+    font-weight: bold;
+}
+
+.background-g {
+    background: rgba(229, 229, 229, 0.5);
+}
+
 </style>
 @endsection
 
 @section('content')
-        <section class="pt-50 pb-50"> 
-            <div class="container">
-                <h1 class="patient-careplan-blue-h1 text-center">Lacey's Care Plan</h1>
-                <div class="row">
+        <section class="pb-50 pt-15px"> 
+        <a class="patient-exit-text d-flex ml-40px" href="{{ route('patient.getstarted') }}" ><i class="material-icons-outlined mt-15px">arrow_back</i>&nbsp;EXIT WAITING ROOM</a>
+                <div class="container max-width-962px">
+                <h1 class="patient-careplan-blue-h1 text-center mt-minus-50px mb-50">Lacey's Care Plan</h1>
+                <div class="row mb-50">
                     <div class="col-md-12">
-                        <!-- Progress Tracker v2 -->
                         <ol class="progressbar">
-                            <li class="active">
+                            <li  class="active">
                                CHECK IN
                             </li>
                             <li>
@@ -66,10 +80,49 @@
                         </ol>
                     </div>
                 </div>
-                <div class="patient-box pt-10px mt-25px">
-                    <div class="row">
-                        
-                    </div>
+                <div class="patient-box mt-25px">
+                <p class="patient-bold-blue-p mb-0px">Before we get Started</p>
+                        <h3 class="waiting-light-blue-h3 mt-minus-25px">Let us know how you've been feeling.</p>
+                        <div class="patient-divider"></div>
+                        <p class="sub-title-p mt-30px">How is your pain today?</p>
+                        <div class="row mt-30px mb-30px">
+                                        <p class="m-auto">Emjoys Coming Soon</p>
+                        </div>
+
+                        <div class="patient-divider"></div>
+                        <p class="sub-title-p mt-30px">Have you had or been in an accident since your last visit?</p>
+
+                        <div class="row mt-10 mb-30px">
+                                        <div class="col-md-12">
+                                            <div class="d-flex align-center">
+                                                <input type="radio" id="male" name="accident" value="1">
+                                                <label for="male" class="custom-16-font mt-10 ml-15px">Yes</label>
+                                            </div>  
+                                            <div class="d-flex align-center">
+                                                <input type="radio" id="female" name="accident" value="2">
+                                                <label for="female" class="custom-16-font mt-10 ml-15px">No</label>
+                                            </div>
+                                        </div>
+                        </div>
+
+                        <div class="patient-divider"></div>
+                        <p class="sub-title-p mt-30px">Do you have any new injuries since your last visit?</p>
+                        <div class="row mt-10 mb-30px">
+                                        <div class="col-md-12">
+                                            <div class="d-flex align-center">
+                                                <input type="radio" id="male" name="injury" value="1">
+                                                <label for="male" class="custom-16-font mt-10 ml-15px">Yes</label>
+                                            </div>  
+                                            <div class="d-flex align-center">
+                                                <input type="radio" id="female" name="injury" value="2">
+                                                <label for="female" class="custom-16-font mt-10 ml-15px">No</label>
+                                            </div>
+                                        </div>
+                        </div>
+
+                        <div class="mb-10 mt-15px">
+                                    <a href="{{ route('patient.waiting.ready') }}"><button class="btn patient-disabled-btn patient-btn-text width-150px height-36px">Submit</button></a>
+                        </div>
                 </div>            
             </div>
         </section>
