2112d418178109aaa5eebc0978299dfd74508d55
peacedev <peacedev711@gmail.com>
2020-10-08 19:52:10 +0200

    login page update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,91 +1,57 @@
-@extends('layouts.app')
+@extends('layouts.auth')
 
 @section('content')
  <!-- ======= Intro Single ======= -->
-    <section class="header-section mb-20">
-        <div class="margin-l-50 margin-r-50 mt-10">
-            <div class="row">
-                <div class="col-md-2 text-center">
-                    <a class="navbar-brand text-brand" href="{{ route('index') }}"><img src="{{ env('APP_LOGO') }}" alt="logo" id="chiroonelogo"></a>
-                </div>
-                <div class="col-md-8">
-                    <h1 class="text-center special-font">Login or Register for Telehealth</h1>
-                </div>
-                <div class="col-md-2 text-center m-auto">
-                </div>
-            </div>
+   
+    <div class="container">
 
-        </div>
-    </section>
-    <!-- End Intro Single-->
-
-    <!-- ======= Login Section ======= -->
-    <div class="intro intro-carousel">
-        <div class="carousel-item-a intro-item bg-image d-flex">
-            <div class="overlay background-g"></div>
-            <div class="m-auto">
-                <div class="container mb-80">
-                    <div class="row">
-
-                        <div class="col-md-1"></div>
-                        <div class="col-md-10 m-auto">
-                            <div class="intro-body">
-                                
-
-                                    <div class="row">
-                                        <div class="col-md-12 mb-20">
-                                            <h1 class="text-center special-font">Welcome!</h1>
-                                        </div>
-                                        <div class="col-md-12">
-                                            <div class="card">
-                                                <article class="card-body">
-                                                <a href="{{ route('register') }}" class="float-right btn btn-outline-primary">Sign up</a>
-                                                <h4 class="card-title mb-4 mt-1">Sign in</h4>
-                                                <form class="form-a" action="{{ route('login') }}" method="POST">
-                                                @csrf
-                                                    <div class="form-group">
-                                                        <label class="color-b">Your email</label>
-                                                        <input type="email" id="email" class="form-control form-control-lg form-control-a @error('email') is-invalid @enderror" placeholder="Your email" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-                                                        @error('email')
-                                                                    <span class="invalid-feedback" role="alert">
-                                                                        <strong>{{ $message }}</strong>
-                                                                    </span>
-                                                        @enderror
-
-                                                    </div>
-
-                                                    <div class="form-group">
-                                                        <label class="color-b">Your password</label>
-                                                        <input type="password" id="password" class="form-control form-control-lg form-control-a @error('password') is-invalid @enderror" placeholder="Your password" name="password" required autocomplete="current-password">
-                                                        @error('password')
-                                                            <span class="invalid-feedback" role="alert">
-                                                                <strong>{{ $message }}</strong>
-                                                            </span>
-                                                        @enderror
-                                                    </div> 
-
-                                                    <br>
-
-                                                    <div class="form-group">
-                                                        <button type="submit" class="btn btn-primary background-green w-100 h-45">Sign In</button>
-                                                    </div>      
-                                                </form>
-                                                </article>
-                                            </div>
-                                        </div>
-                                         
-                                     
-                                    </div>
-                               
-                            </div>
-
-                           
-                        </div>
-                         <div class="col-md-1"></div>
+                <div class="row mt-40px mb-50px">
+                    <div class="col-md-12 text-center">
+                        <a href="{{ route('index') }}"><img class="height-110px" src="{{ asset('admin_assets/dist/img/PatientConnectLogo.png') }}" alt="logo" id="chiroonelogo"></a>
                     </div>
                 </div>
-            </div>
-        </div>
+
+                <div class="row">
+                        <div class="col-md-6 m-auto text-center">
+                            <h1 class="patient-bold-h1-font mb-20px">Login or Sign Up</h1>
+                            <form class="form-a" action="{{ route('login') }}" method="POST">
+                                @csrf
+                                    <div class="form-group pl-90px pr-90px">
+                                        <input type="email" id="email" class="form-control form-control-lg form-control-a @error('email') is-invalid @enderror height-59px custom-16-font" placeholder="Email" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                        @error('email')
+                                                    <span class="invalid-feedback" role="alert">
+                                                        <strong>{{ $message }}</strong>
+                                                    </span>
+                                        @enderror
+                                    </div>
+
+                                    <div class="form-group mb-0 pl-90px pr-90px">
+                                        <input type="password" id="password" class="form-control form-control-lg form-control-a @error('password') is-invalid @enderror height-59px custom-16-font" placeholder="Password" name="password" required autocomplete="current-password">
+                                        @error('password')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div> 
+                                    <a href="#" class="login-link-small-font">Forgot your password?</a>
+                                    <div class="form-group mt-20px">
+                                        <button type="submit" class="btn btn-blue custom-btn-text width-328px height-36px">Sign In</button>
+                                    </div>      
+                            </form>
+
+                            <p class="custom-16-font">New around here? <a href="{{ route('register') }}" class="color-lightblue">Create your account</a></p>
+                        </div>
+                        <div class="col-md-6">
+                            <img class="width-100" src="{{ asset('admin_assets/dist/img/PatientConnectAvatar1.jpg') }}" alt="diagnos image">
+                        </div>
+                </div>
+
     </div>
-    <!-- End Login Section -->
+                
+ 
+    <!-- End Intro Single-->
+
+
+                    
+
 @endsection
---
+++ resources/views/layouts/auth.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<!--
+This is a starter template page. Use this page to start your new project from
+scratch. This page gets rid of all links and provides the needed markup only.
+-->
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+  <title>Telehealth PatientConnect | Admin Dashboard</title>
+
+  <!-- Font Awesome Icons -->
+  <!-- <link rel="stylesheet" href="{{ asset('admin_assets/plugins/fontawesome-free/css/all.min.css') }}"> -->
+  <link href="https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp" rel="stylesheet">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="{{ asset('admin_assets/dist/css/adminlte.min.css') }}">
+
+  <!-- Toastr -->
+  <link rel="stylesheet" href="{{ asset('admin_assets/plugins/toastr/toastr.min.css') }}">
+
+  <!-- Google Font: Source Sans Pro -->
+  <!-- <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet"> -->
+  <link rel="stylesheet" href="https://use.typekit.net/jbm8ofu.css">
+  <link rel="stylesheet" href="{{ asset('admin_assets/patient.css') }}">
+  @yield('css')
+</head>
+<body class="hold-transition sidebar-mini">
+        
+        @yield('content')
+
+
+<!-- jQuery -->
+<script src="{{ asset('admin_assets/plugins/jquery/jquery.min.js') }}"></script>
+<!-- Bootstrap 4 -->
+<script src="{{ asset('admin_assets/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<!-- AdminLTE App -->
+<script src="{{ asset('admin_assets/dist/js/adminlte.min.js') }}"></script>
+<script src="{{ asset('admin_assets/plugins/toastr/toastr.min.js') }}"></script>
+
+@yield('javascript')
+
+</body>
+</html>
